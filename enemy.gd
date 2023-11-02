885b4b7f40391203b2915284329dbeb810a7fa00
QuapperWasTaken <149621949+QuapperWasTaken@users.noreply.github.com>
2023-11-02 12:14:54 +0100

    Fixed issue #3

---
+++ enemy.gd
@@ -0,0 +1,19 @@
+extends Sprite2D
+
+var speed = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if position.x >= $"/root/main/Tower".position.x:
+		position.x -= speed
+	else:
+		position.x += speed
+	if position.y >= $"/root/main/Tower".position.y:
+		position.y -= speed
+	else:
+		position.y += speed
